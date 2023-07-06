550587a18d2aab0836e145fba7cfa276ed2a4054
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2023-07-06 22:25:47 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+    int grade;
+    cin>>grade;
+    if(grade>=90)
+        cout<<'A';
+    else if(grade>=80)
+        cout<<'B';
+    else if(grade>=70)
+        cout<<'C';
+    else if(grade>=60)
+        cout<<'D';
+    else
+        cout<<'F';
+}

4fddb4aaae85a805718e957b77b634fc3bd30dd5
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2023-07-07 18:38:27 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.cc
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -2,16 +2,29 @@
 #include<vector>
 #include<set>
 using namespace std;
-
 int main() {
     ios::sync_with_stdio(NULL);
     cin.tie(NULL);
     cout.tie(NULL);
+    /* Using set.
     set<int>s;
     for(int i=0;i<10;i++){
         int input;cin>>input;
         s.insert(input%42);
     }
     cout<<s.size()<<"\n";
+*/
+    //Using array
+    int *arr = new int[42];
+    for(int i=0;i<10;i++){
+        int input;cin>>input;
+        arr[input%42]++;
+    }
+    int cnt=0;
+    for(int i=0;i<42;i++){
+        if(arr[i]!=0)
+            cnt++;
+    }
+    cout<<cnt<<"\n";
 
 }

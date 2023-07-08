fa9a6b9750cba34c8bf206c8a4bb827489d4d2e2
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2023-07-08 17:08:56 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"         ,r'\"7\n";
+    cout<<"r`-_   ,'  ,/\n";
+    cout<<" \\. \". L_r'\n";
+    cout<<"   `~\\/\n";
+    cout<<"      |\n";
+    cout<<"      |\n";
+
+}

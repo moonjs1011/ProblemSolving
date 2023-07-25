b4ca0675c8dfdf15629422e92a3d2b501dcf83ad
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2023-07-25 19:36:48 +0900

    [Silver III] Title: 고양이 카페, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/28353. 고양이 카페/고양이 카페.cc
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.cc
@@ -1,33 +1,39 @@
-#include <iostream>
-#include<vector>
+#include<iostream>
 #include<algorithm>
 using namespace std;
+
+int * init_list(int N){ // init input & return array
+    int *arr = new int[N];
+    for(int i=0;i<N;i++)
+        cin>>arr[i];
+    sort(arr,arr+N);
+    return arr;
+}
+
+bool * init_check(int N){ // return check array
+    bool *arr = new bool[N];
+    for(int i=0;i<N;i++){
+        arr[i]=false;
+    }
+    return arr;
+}
+
 int main(){
     int N,K;
     cin>>N>>K;
-    vector<int> v;
-    vector<bool>check;
-    for(int i=0;i<N;i++){
-        int input;
-        cin>>input;
-        v.push_back(input);
-        check.push_back(false);
-    }
-    sort(v.begin(),v.end());
+    int *list=init_list(N);
+    bool *check = init_check(N);
     int cnt=0;
     for(int i=0;i<N;i++){
-        int max_j=-1;
-        for(int j=0;j<N;j++){
-            if(i==j) continue;
-            if(!check[i] && !check[j] &&(v[i]+v[j])<=K){
-                max_j = max(j,max_j);
+        int max_index=-1;
+        for(int j=i+1;j<N;j++){
+            if(!check[i] && !check[j] && list[i]+list[j]<=K){
+                max_index= max(max_index,j);
             }
         }
-        if(max_j!=-1){
-            check[i]=true;
-            check[max_j]=true;
+        if(max_index!=-1){
+            check[i]=check[max_index]=true;
             cnt++;
-
         }
     }
     cout<<cnt<<"\n";

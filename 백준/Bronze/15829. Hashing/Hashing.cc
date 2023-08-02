911edbfefeafb20c9c8f745ed1de3e3d825bf28f
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2023-08-02 14:48:55 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -6,14 +6,13 @@ int main(){
     int len;
     cin>>len;
     long long sum=0;
-    int r=1;
-    int M = 1234567891;
+    long long r=1;
+    long long  M = 1234567891;
     string num;
     cin>>num;
     for(int i=0;i<num.size();i++){
-        sum+= (num[i]-96)* r;
+        sum= (sum+ (num[i]-96)* r)%M;
         r = (r* 31) % M;
     }
-    long long res = sum%M;
-    cout<<res<<"\n";
+    cout<<sum<<"\n";
 }

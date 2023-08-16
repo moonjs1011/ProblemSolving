d3e4688ec0e01c9ce8da4bab996bc688c2c3284c
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2023-08-16 15:57:37 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 212 ms, Memory: 23944 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<map>
+
+using namespace std;
+map<int, string> map_int_str;
+map<string, int> map_str_int;
+void find_name(int index){
+    cout<<map_int_str[index]<<"\n";
+    return ;
+}
+void find_index(string name){
+    cout<<map_str_int[name]<<"\n";
+    return;
+}
+int main() {
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N, M;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        string name;
+        cin >> name;
+        map_int_str.insert({i, name});
+        map_str_int.insert({name, i});
+    }
+    while(M--){
+        string input;
+        cin>>input;
+        if(input[0]-'0'>=1 && input[0] -'0' <=9){
+            find_name(stoi(input));
+        }
+        else find_index(input);
+    }
+}

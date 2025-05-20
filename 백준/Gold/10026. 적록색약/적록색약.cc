14e64c21f368a3f209a9cb3ce62013272a355145
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2025-05-20 16:02:59 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,70 +1,66 @@
-#include <iostream>
-#include<cstring>
+#include<iostream>
 #include<queue>
+#include<cstring>
+
 #define MAXSIZE 101
 using namespace std;
 int N;
 char board[MAXSIZE][MAXSIZE];
 bool visited[MAXSIZE][MAXSIZE];
-int dy[]={-1,1,0,0};
-int dx[] = {0,0,1,-1};
-vector<vector<pair<int,int>>>rgb(3); //위치 저장
-//범위 체크
-bool range_check(int y,int x){
-    return y>=0 && y<N && x>=0 && x<N;
+int dy[] = {-1, 1, 0, 0};
+int dx[] = {0, 0, 1, -1};
+
+bool rangeCheck(int y, int x) {
+    return y >= 0 && y < N && x >= 0 && x < N;
 }
 
-int bfs(int y,int x){
-    if(visited[y][x]) //이미 방문했을 경우
+int bfs(int y, int x, char ch) {
+    if (visited[y][x])
         return 0;
+    queue<pair<int, int>> q;
     visited[y][x] = true;
-    queue<pair<int,int>> q;
-    q.push({y,x});
-    while(!q.empty()){
+    q.push({y, x});
+    while (!q.empty()) {
         int cur_y = q.front().first;
         int cur_x = q.front().second;
-        int cur_c = board[cur_y][cur_x]; //current_color
         q.pop();
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             int next_y = cur_y + dy[i];
             int next_x = cur_x + dx[i];
-            if(range_check(next_y,next_x) && !visited[next_y][next_x]&&board[next_y][next_x]==cur_c){
-                visited[next_y][next_x]=true;
-                q.push({next_y,next_x});
+            if (rangeCheck(next_y, next_x) && board[next_y][next_x] == ch && !visited[next_y][next_x]) {
+                visited[next_y][next_x] = true;
+                q.push({next_y, next_x});
             }
         }
     }
     return 1;
 }
-//BFS2(적록색약인 사람)
-int main(){
-    //Input board info
-    cin>>N;
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            cin>>board[i][j];
-        }
-    }
-    memset(visited, false,sizeof(visited));
-    int normal_count =0;
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            normal_count+=bfs(i,j);
-        }
-    }
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            if(board[i][j]=='G')
-                board[i][j] ='R';
-        }
-    }
-    memset(visited, false,sizeof(visited));
-    int half_count = 0;
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            half_count+=bfs(i,j);
-        }
-    }
-    cout<<normal_count<<" "<<half_count<<"\n";
-}
 
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> board[i][j];
+        }
+    }
+    memset(visited, 0, sizeof(visited));
+    int normal_cnt = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            normal_cnt += bfs(i, j, board[i][j]);
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (board[i][j] == 'G') board[i][j] = 'R';
+        }
+    }
+    memset(visited, 0, sizeof(visited));
+    int odd_cnt = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            odd_cnt += bfs(i, j, board[i][j]);
+        }
+    }
+    cout << normal_cnt << " " << odd_cnt << "\n";
+}

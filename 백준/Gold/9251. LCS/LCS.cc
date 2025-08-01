6eaeb0f1939a2da45d1a169071d2bcb7e6e77ea4
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2025-08-01 23:45:31 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5984 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -6,9 +6,10 @@
 
 using namespace std;
 
-void lcs(string str1, string str2) {
+int lcs(string str1, string str2) {
     //0으로 초기화
     vector<vector<int>> dict(str1.size() + 1, vector<int>(str2.size() + 1, 0));
+    int ans = 0;
     for (int i = 1; i < dict.size(); i++) {
         for (int j = 1; j < dict[i].size(); j++) {
             if (str1[i - 1] == str2[j - 1]) {
@@ -16,20 +17,15 @@ void lcs(string str1, string str2) {
             } else {
                 dict[i][j] = max(dict[i - 1][j], dict[i][j - 1]);
             }
+            ans = max(dict[i][j], ans);
         }
     }
-    int ans = 0;
-    for (int i = 0; i < dict.size(); i++) {
-        for (int j = 0; j < dict[i].size(); j++) {
-            if (ans < dict[i][j])
-                ans = dict[i][j];
-        }
-    }
-    cout << ans;
+    return ans;
 }
 
 int main() {
     string str1, str2;
     cin >> str1 >> str2;
-    lcs(str1, str2);
+    cout<<lcs(str1, str2)<<"\n";
+    return 0;
 }

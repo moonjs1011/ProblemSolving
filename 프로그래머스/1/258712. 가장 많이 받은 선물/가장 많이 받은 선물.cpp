80e517e56585c18a13a68db86f1637dba8839ea2
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2025-09-02 19:13:19 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 16.86 ms, Memory: 4.39 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,57 +1,73 @@
 #include <string>
-#include <sstream>
 #include <vector>
-#include <map>
-#include <iostream>
-#include <algorithm>
+#include<map>
+#include<sstream>
+
 using namespace std;
 
-map<string, int> giftScore(vector<string> friends, vector<string> gifts) {
-    map<string, int> score;                // giftScore
-    map<string, map<string, int>> history; // A->B 몇 개 줬는지
-    map<string, int> next_month;
 
-    for (auto &f : friends) {
-        score[f] = 0;
-        next_month[f] = 0;
-    }
+int solution(vector<string> friends, vector<string> gifts) {
+    map<string, map<string, int>> doubleMap;
+    map<string, map<string, bool>> checked;
+    map<string, int> giftScore;
+    map<string, int> nextMonth;
 
-    for (auto &g : gifts) {
-        istringstream iss(g);
-        string a, b; iss >> a >> b;
-        score[a]++; score[b]--;
-        history[a][b]++;
-    }
-
-    // 친구 쌍을 한 번만 비교
-    for (int i = 0; i < friends.size(); i++) {
-        for (int j = i + 1; j < friends.size(); j++) {
-            string a = friends[i];
-            string b = friends[j];
-
-            int ab = history[a][b];
-            int ba = history[b][a];
-
-            if (ab > ba) {
-                next_month[a]++;
-            } else if (ab < ba) {
-                next_month[b]++;
-            } else {
-                if (score[a] > score[b]) next_month[a]++;
-                else if (score[a] < score[b]) next_month[b]++;
-                // 같으면 둘 다 없음
+    for (string s1: friends) { //init
+        for (string s2: friends) {
+            if (s1 != s2) {
+                doubleMap[s1][s2] = 0;
+                giftScore[s1] = 0;
+                nextMonth[s1] = 0;
+                checked[s1][s2] = false;
             }
         }
     }
-
-    return next_month;
-}
-
-int solution(vector<string> friends, vector<string> gifts) {
-    auto next_month = giftScore(friends, gifts);
-    int answer = 0;
-    for (auto &kv : next_month) {
-        answer = max(answer, kv.second);
+    for (string s: gifts) {
+        istringstream iss(s);
+        string name1;
+        iss >> name1;
+        string name2;
+        iss >> name2;
+        doubleMap[name1][name2] += 1;
+        giftScore[name1] += 1;
+        giftScore[name2] -= 1;
     }
-    return answer;
+
+    for (auto e: doubleMap) {
+        for (auto kv: e.second) {
+            string person1 = e.first;
+            string person2 = kv.first;
+
+            // Skip if this pair has already been processed
+            if (checked[person1][person2]) {
+                continue;
+            }
+
+            // Mark the pair as processed to avoid double-counting
+            checked[person1][person2] = true;
+            checked[person2][person1] = true;
+
+            int p1_gave_p2 = doubleMap[person1][person2];
+            int p2_gave_p1 = doubleMap[person2][person1];
+
+            // Rule 1: Compare gift counts
+            if (p1_gave_p2 > p2_gave_p1) {
+                nextMonth[person1]++; // person1 gets a gift
+            } else if (p2_gave_p1 > p1_gave_p2) {
+                nextMonth[person2]++; // person2 gets a gift
+            } else {
+                // Rule 2: If counts are equal, compare gift scores
+                if (giftScore[person1] > giftScore[person2]) {
+                    nextMonth[person1]++;
+                } else if (giftScore[person2] > giftScore[person1]) {
+                    nextMonth[person2]++;
+                }
+            }
+        }
+    }
+    int maxCount = 0;
+    for (auto e: nextMonth) {
+        maxCount = max(maxCount, e.second);
+    }
+    return maxCount;
 }

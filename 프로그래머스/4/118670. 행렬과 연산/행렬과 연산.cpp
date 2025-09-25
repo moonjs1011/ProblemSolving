da57fe8f99c6a187b1e01b3362a892416d273a72
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2025-09-25 14:24:50 +0900

    [level 4] Title: 행렬과 연산, Time: 67.09 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/4/118670. 행렬과 연산/행렬과 연산.cpp
+++ 프로그래머스/4/118670. 행렬과 연산/행렬과 연산.cpp
@@ -1,52 +1,49 @@
-#include <bits/stdc++.h>
+
+#include <string>
+#include <vector>
+#include <deque>
 using namespace std;
 
+int N, M;
+
 vector<vector<int>> solution(vector<vector<int>> rc, vector<string> operations) {
-    int r = rc.size();
-    int c = rc[0].size();
-    deque<int> col1, col2;
-    for(int i = 0; i < r; i++){
-        col1.push_back(rc[i][0]);
-        col2.push_back(rc[i][c-1]);
+    deque<int> left_queue;
+    deque<int> right_queue;
+
+    deque<deque<int>*> dq;
+    N = rc.size(), M = rc[0].size();
+
+    left_queue.assign(N, 0);
+    right_queue.assign(N, 0);
+
+    for (int i = 0; i < N; i++)
+        dq.push_back(new deque<int>(M - 2));
+
+    for (int i = 0; i < N; i++) {
+        left_queue[i] = rc[i][0];
+        right_queue[i] = rc[i][M - 1];
+        for (int j = 1; j < M - 1; j++)
+            dq[i]->at(j - 1) = rc[i][j];
+     }
+
+    for (string s : operations) {
+        if (s[0] == 'S') {
+            dq.push_front(dq.back()); dq.pop_back();
+            left_queue.push_front(left_queue.back()); left_queue.pop_back();
+            right_queue.push_front(right_queue.back()); right_queue.pop_back();
+        } else {
+            dq[0]->push_front(left_queue.front()); left_queue.pop_front();
+            right_queue.push_front(dq[0]->back()); dq[0]->pop_back();
+            dq[N - 1]->push_back(right_queue.back()); right_queue.pop_back();
+            left_queue.push_back(dq[N - 1]->front()); dq[N - 1]->pop_front();
+        }
     }
 
-    deque<int> rows[r];
-    for(int i = 0; i < r; i++)
-        for(int j = 1; j <= c-2; j++)
-            rows[i].push_back(rc[i][j]);
-    
-    int idx = 0;
-    for(auto op : operations){
-        if(op == "ShiftRow"){
-            col1.push_front(col1.back());
-            col1.pop_back();
-            col2.push_front(col2.back());
-            col2.pop_back();
-            idx--;
-            if(idx == -1) idx = r-1;
-        }
-        else{ // Rotate
-            rows[idx].push_front(col1.front());
-            col1.pop_front();
-            
-            col2.push_front(rows[idx].back());
-            rows[idx].pop_back();
-            
-            rows[(idx+r-1)%r].push_back(col2.back());
-            col2.pop_back();
-            
-            col1.push_back(rows[(idx+r-1)%r].front());
-            rows[(idx+r-1)%r].pop_front();
-        }
+    for(int i = 0; i < N; i++) {
+        rc[i][0] = left_queue[i];
+        rc[i][M - 1] = right_queue[i];
+        for(int j = 1; j < M - 1; j++) rc[i][j] = dq[i]->at(j-1);
     }
-    
-    
-    vector<vector<int>> ret(r, vector<int>(c));
-    for(int i = 0; i < r; i++){
-        ret[i][0] = col1[i];
-        for(int j = 1; j <= c-2; j++)
-            ret[i][j] = rows[(i + idx) % r][j-1];
-        ret[i][c-1] = col2[i];
-    }
-    return ret;
+    return rc;
+
 }

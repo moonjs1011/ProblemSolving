99f0259815d97a0cea8563fcb4577b779406aaa8
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2025-10-17 01:41:05 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 204 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,38 +1,33 @@
+//
+// Created by Sung_1 on 2025. 10. 17..
+//
 #include<iostream>
-#include<string>
 #include<map>
 
 using namespace std;
-map<int, string> map_int_str;
-map<string, int> map_str_int;
-
-void find_name(int index) {
-    cout << map_int_str[index] << "\n";
-    return;
-}
-
-void find_index(string name) {
-    cout << map_str_int[name] << "\n";
-    return;
-}
 
 int main() {
-    ios::sync_with_stdio(NULL);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int N, M;
     cin >> N >> M;
+    map<int, string> intStrMap;
+    map<string, int> strIntMap;
     for (int i = 1; i <= N; i++) {
         string name;
         cin >> name;
-        map_int_str.insert({i, name});
-        map_str_int.insert({name, i});
+        intStrMap[i] = name;
+        strIntMap[name] = i;
     }
     while (M--) {
-        string input;
-        cin >> input;
-        if (input[0] - '0' >= 1 && input[0] - '0' <= 9) {
-            find_name(stoi(input));
-        } else find_index(input);
+        string question;
+        cin >> question;
+        if (question[0] >= 'A' && question[0] <= 'Z') {// input이 이름인 경우, strIntMap 접근
+            cout << strIntMap[question] << "\n";
+        } else { //input이  숫자인 경우, intStrMap 접근
+            int num = stoi(question);
+            cout << intStrMap[num] << "\n";
+        }
     }
 }

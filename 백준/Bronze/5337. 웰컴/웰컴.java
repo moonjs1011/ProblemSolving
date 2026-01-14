92ad98a109b19285f3b20e25900ce3b849c1bb32
Sung_1 <87971454+moonjs1011@users.noreply.github.com>
2026-01-14 09:21:29 +0900

    [Bronze V] Title: 웰컴, Time: 88 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(".  .   .");
+		bw.newLine();
+		bw.write("|  | _ | _. _ ._ _  _");
+		bw.newLine();
+		bw.write("|/\\|(/.|(_.(_)[ | )(/.");
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
